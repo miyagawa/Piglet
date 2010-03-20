143f13a1b5781cc7b81d4c144135c7e1d11ac4a7
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-20 02:13:04 -0700

    Implemented very basic Piglet::Routes = Router::Simple + magic path_info rewriting

---
+++ lib/Piglet/Routes.pm
@@ -0,0 +1,76 @@
+package Piglet::Routes;
+use strict;
+use Router::Simple;
+
+sub new {
+    my $class = shift;
+    bless { rs => Router::Simple->new }, $class;
+}
+
+sub connect { shift->{rs}->connect(@_) }
+
+sub match {
+    my($self, $env) = @_;
+
+    my $m = $self->{rs}->match($env) or return;
+
+    # magic path_info
+    if ($m->{path_info}) {
+        if ($env->{PATH_INFO} =~ s!(.*?)(/?)\Q$m->{path_info}\E$!!) {
+            $m->{path_info}      = $2 . $m->{path_info};
+            $env->{SCRIPT_NAME} .= $1;
+            $env->{PATH_INFO}    = $m->{path_info};
+        } else {
+            # Hmm what should it do?
+        }
+    }
+
+    return $m;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Piglet::Routes - Define routes and register web handlers
+
+=head1 SYNOPSIS
+
+  use Piglet::Routes;
+
+=head1 DESCRIPTION
+
+Piglet::Routes is a simple wrapper around L<Router::Simple> that adds
+more integration with PSGI application environment.
+
+=head1 FEATURES
+
+=head2 Magic path_info
+
+  $r->connect('/foo/{path_info:.*}', { app => $app });
+
+  my $match = $r->match($env); # /foo/bar/baz
+
+  # $env->{SCRIPT_NAME} = "/foo"
+  # $env->{PATH_INFO}   = "/bar/baz"
+
+If the matched parameter contains the special key C<path_info>, it
+rewrites C<SCRIPT_NAME> and C<PATH_INFO> so the delegated PSGI
+application behaves like it's mounted under the path, similarly to
+L<Plack::App::URLMap>.
+
+Note that in the example, route is defined as C</foo/{path_info:.*}>
+and B<NOT> C</foo{path_info:.*}>. The latter also works but it would
+also match with C</foobar/baz> which would result in an unwanted
+behavior.
+
+Inspired by routes.rb's magic path_info
+handling. L<http://routes.groovie.org/setting_up.html#magic-path-info>
+
+=head1 SEE ALSO
+
+L<Piglet> L<Router::Simple>
+
+=cut
---
+++ t/routes/simple.t
@@ -0,0 +1,39 @@
+use strict;
+use Piglet::Routes;
+use Test::More;
+
+use HTTP::Message::PSGI;
+use HTTP::Request::Common;
+
+sub env {
+    HTTP::Message::PSGI::req_to_psgi(GET @_);
+}
+
+my $r = Piglet::Routes->new;
+$r->connect("/users/list", { controller => "Users", action => "list" });
+$r->connect("/{user:[a-z0-9_]+}", { controller => "Profile" });
+$r->connect("/admin/{path_info:.*}", { app => "Admin" });
+
+{
+    my $m = $r->match( env "/users/list" );
+    is_deeply $m, { controller => "Users", action => "list" };
+}
+
+{
+    my $m = $r->match( env "/miyagawa" );
+    is_deeply $m, { controller => "Profile", user => "miyagawa" };
+}
+
+{
+    my $env = env "/admin/login/me";
+    is $env->{SCRIPT_NAME}, '';
+    is $env->{PATH_INFO}, '/admin/login/me';
+
+    my $m = $r->match($env);
+    is_deeply $m, { app => "Admin", path_info => "/login/me" };
+    is $env->{SCRIPT_NAME}, '/admin';
+    is $env->{PATH_INFO}, '/login/me';
+}
+
+done_testing;
+
